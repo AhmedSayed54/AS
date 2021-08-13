04eb001b8ddb578ff502072ce83b51073a013130
AhmedSayed54 <asayed.as954@gmail.com>
2021-08-13 14:42:54 +0200

    Revert "task3 "
    This reverts commit a17b2f396f7425d300223799e51faeeab1b18e5a.
    it  is reverted to rename it.

--- task3.py
+++ task3.py
@@ -1,9 +1,10 @@
 
 numbers_list = [5, 26, 45, 32, 63, 74, 81, 120, 99, 54]
-print("The numbers from the list", numbers_list, "are divisible by 9:")
 
 
 def div_by_9():
+    print("The numbers from the list", numbers_list, "are divisible by 9:")
+
     for sol in numbers_list:
         if sol % 9 == 0:
             print(sol)
