4528b8642fb02ac91390399db52ab70ca1be2df0
AhmedSayed54 <asayed.as954@gmail.com>
2021-08-13 14:36:45 +0200

    task3 v1

---
+++ task3.py
@@ -0,0 +1,13 @@
+
+numbers_list = [5, 26, 45, 32, 63, 74, 81, 120, 99, 54]
+
+
+def div_by_9():
+    print("The numbers from the list", numbers_list, "are divisible by 9:")
+
+    for sol in numbers_list:
+        if sol % 9 == 0:
+            print(sol)
+
+
+print(div_by_9())
